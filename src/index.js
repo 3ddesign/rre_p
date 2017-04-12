4e34db363cf258d34fbc1712e7f131f6612c889e
Dmitry Scherbakov <3ddesign@bk.ru>
2017-04-12 11:07:43 +0300

    rre_p 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('Hi');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends  React.Component {
+    render() {
+
+        return (
+            <h1>App works!</h1>
+        );
+
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
+
+console.log('Hi!');
