b9d5ace2f949fb99366425415f000d0db40ec487
Dmitry Scherbakov <3ddesign@bk.ru>
2017-04-12 12:19:31 +0300

    rre_p 1

--- src/index.js
+++ src/index.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { PropTypes, Component } from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
+class App extends Component {
+
+    static propTypes = {
+        btnText: PropTypes.string.isRequired,
+        h1text: PropTypes.string.isRequired,
+        newArray: PropTypes.array.isRequired
+    };
+
+    static defaultProps = {
+        btnText: 'default props text'
+    };
+
+    btnClick() {
+        console.log('Hi!');
+    }
+
     render() {
+        console.log(this.props.newArray);
         return (
-            <div>
-                <h1>App works!</h1>
-                <h3>It`s really working!</h3>
+            <div className="test">
+                <h1> {this.props.h1text }</h1>
+                <h3>It`s really working!!!</h3> 
+                <button onClick={this.btnClick}>{ this.props.btnText }</button>
             </div>
         );
     }
 }
 
 ReactDOM.render(
-    <App />,
+    <App h1text="This is H1 text" newArray={[1,2,3]}/>,
     document.getElementById('app')
 );
 
-
-console.log('Hi!');
 
