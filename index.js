54632cc52318d957da4bbebbad9088c8cc0b7c11
3ddesign <3ddesign@bk.ru>
2018-01-28 21:25:23 +0200

    test

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const http = require('http');
-const app = require('./app');
+// const http = require('http');
+// const app = require('./app');
+//
+// const server = http.createServer(app);
 
-const server = http.createServer(app);
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'test' });
+});
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT);
+// server.listen(PORT);
+app.listen(PORT);
