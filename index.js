4c9fc7de78f9048c330b61b4260d96cd29bb1a7f
3ddesign <3ddesign@bk.ru>
2018-07-30 08:01:07 +0300

    test

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-// const http = require('http');
+const http = require('http');
+http.createServer(function(req, res) {
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('test\n');
+}).listen(8080, '127.0.0.1');
+
+
 // const app = require('./app');
 //
-// const server = http.createServer(app);
 
-const express = require('express');
-const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-});
-
-const PORT = process.env.PORT || 5000;
-// server.listen(PORT);
-app.listen(PORT);
+// const express = require('express');
+// const app = express();
+//
+// app.get('/', (req, res) => {
+//     res.send({ hi: 'there' });
+// });
+//
+// const PORT = process.env.PORT || 5000;
+// // server.listen(PORT);
+// app.listen(PORT);
