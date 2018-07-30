470ec67354294d72b40e46d9e6a78e4d2ebb6ee7
3ddesign <3ddesign@bk.ru>
2018-07-30 08:06:22 +0300

    test

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const http = require('http');
-http.createServer(function(req, res) {
-
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('test\n');
-}).listen(8080, '127.0.0.1');
+// http.createServer(function(req, res) {
+//
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end('test\n');
+// }).listen(50, '127.0.0.1');
 
 
 // const app = require('./app');
 //
 
 
-// const express = require('express');
-// const app = express();
-//
-// app.get('/', (req, res) => {
-//     res.send({ hi: 'there' });
-// });
-//
-// const PORT = process.env.PORT || 5000;
-// // server.listen(PORT);
-// app.listen(PORT);
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('test\n');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
