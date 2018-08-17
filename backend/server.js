f48f64087c188b6de9a40b9ce6b6a92c8138ea2e
3ddesign <3ddesign@bk.ru>
2018-08-17 08:11:17 +0300

    server implementation

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,54 @@ connection.once('open', () => {
     console.log('MongoDB connection established sucessfully');
 });
 
-router.route('/issue').get(req, res)
+router.route('/issues').get((req, res) => {
+    Issue.find((err, issues) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.json(issues);
+        }
+    })
+});
+
+router.route('/issues/:id').get((req, res) => {
+    Issue.findById(req.params.id, (err, issue) => {
+        if (err)
+            console.log(err);
+        else
+            res.json(issue);
+    })
+});
+
+router.route('/issues/add').post((req, res) => {
+    let issue = new Issue(req.body);
+    issue.save()
+        .then(issue => {
+            res.status(200).json({'issue': 'Added successfully'});
+        })
+        .catch(err => {
+            res.status(400).send('Failed to create new record');
+        });
+});
+
+router.route('/issues/update/:id').post((req, res) => {
+    Issue.findById(req.params.id, (err, issue) => {
+        if (!issue)
+            return next(new Error('Could not load Document'));
+        else {
+            issue.title = req.body.title;
+            issue.responsible = req.body.responsible;
+            issue.description = req.body.description;
+            issue.severity = req.body.severity;
+            issue.status = req.body.status;
+            issue.save().then(issue => {
+                res.json('Update done');
+            }).catch(err => {
+                res.status(400).send('Update failed');
+            });
+        }
+    });
+});
 
 app.use('/', router);
 
